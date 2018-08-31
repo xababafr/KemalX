77853dd5c6b771c6c141b6de06995fe3224ebb09
xaba <xavier.quelard@gmail.com>
2018-08-31 05:26:54 +0200

    premier exemple de MVC complet! Les modeles sont juste des collections de méthodes statiques, normalement, c'est tout a fait logique

--- src/controllers/root_controller.cr
+++ src/controllers/root_controller.cr
@@ -1,7 +1,8 @@
 class RootController < KemalX::Controller
 
   def root(env)
-    name = env.params.url["name"]
+    nbr = RootModel.get2 3
+    name = "#{env.params.url["name"]}_#{nbr}"
     render "src/views/root/hello.ecr", "src/views/layout.ecr"
   end
 
--- src/models/root_model.cr
+++ src/models/root_model.cr
@@ -1,3 +1,11 @@
 class RootModel < KemalX::Model
 
+  def self.get2(i)
+    if i > 2
+      i
+    else
+      2
+    end
+  end
+
 end
